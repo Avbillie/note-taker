d88005a878a6340f19d1d124b60ae738b3819cd0
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2020-12-05 18:19:52 -0500

    added start script

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-require("../Develop/public/routes/apiRoutes")(app);
-require("../Develop/public/routes/htmlRoutes")(app);
+require("./public/routes/apiRoutes.js")(app);
+require("./public/routes/htmlRoutes")(app);
 
 app.listen(PORT,function(){
     console.log("App listening on PORT " + PORT);
